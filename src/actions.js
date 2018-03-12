067205d3e5fd8dc3bab13468385b73ff5495bbb1
David Kindler <david.kindler@nxp.com>
2018-03-11 22:26:28 -0500

    add hoc

---
+++ src/actions.js
@@ -0,0 +1,22 @@
+import AppDispatcher from './AppDispatcher';
+
+export const updateNumberOfPeople = value => {
+  const action = {
+    type: 'UPDATE_NUMBER_OF_PEOPLE',
+    value: value
+  };
+  AppDispatcher.dispatch(action);
+};
+
+export const updateSlicesPerPerson = value => {
+  AppDispatcher.dispatch({
+    type: 'UPDATE_SLICES_PER_PERSON',
+    value: value
+  });
+};
+
+export const reset = () => {
+  AppDispatcher.dispatch({
+    type: 'RESET'
+  });
+};
